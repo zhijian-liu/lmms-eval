Fix llava onevision loglikelihood video bug

lmms_eval/models/llava_onevision.py
@@ -260,64 +260,64 @@ class Llava_OneVision(lmms):
         pbar = tqdm(total=len(requests), disable=(self.rank != 0), desc="Model Responding")
 
         for contexts, doc_to_target, doc_to_visual, doc_id, task, split in [reg.args for reg in requests]:
-            visual = doc_to_visual(self.task_dict[task][split][doc_id])
-
-            if visual is None or visual == []:
-                visual = None
-                task_type = "text"
-                image_tensor = None
+            # encode, pad, and truncate contexts for this batch
+            if type(doc_to_target) == str:
+                continuation = doc_to_target
             else:
-                if len(visual) > 1 or "image_aspect_ratio" not in self._config.__dict__:
-                    self._config.image_aspect_ratio = "pad"
-                    eval_logger.info(f"In Multi-Image setting, image aspect ratio: {self._config.image_aspect_ratio}")
+                continuation = doc_to_target(self.task_dict[task][split][doc_id])
+            visuals = [doc_to_visual(self.task_dict[task][split][doc_id])]
+            visuals = self.flatten(visuals)
+            if visuals:
+                for visual in visuals:
+                    if len(visual) > 1 or "image_aspect_ratio" not in self._config.__dict__:  # for multi image case, we treat per image aspect ratio as "pad" by default.
+                        self._config.image_aspect_ratio = "pad"
+                        eval_logger.info(f"Setting image aspect ratio: {self._config.image_aspect_ratio}")
+                    # if (len(visual) > 1 or "image_aspect_ratio" not in self._config.__dict__) and ("image_aspect_ratio" in gen_kwargs.keys()):
+                    #     self._config.image_aspect_ratio = gen_kwargs["image_aspect_ratio"]
+                    #     eval_logger.info(f"Setting image aspect ratio: {self._config.image_aspect_ratio}")
 
-                if "task_type" in self.metadata and self.metadata["task_type"] == "video" and "sample_frames" in self.metadata:
-                    assert type(visual) == list, "sample_frames must be specified for video task"
-                    sample_indices = np.linspace(0, len(visual) - 1, self.metadata["sample_frames"], dtype=int)
-                    visual = [visual[i] for i in sample_indices]
-                    assert len(visual) == self.metadata["sample_frames"]
+                    if type(visual) == PIL.Image.Image:  # For image task
+                        image = process_images([visual], self._image_processor, self._config)
+                        if type(image) is list:
+                            image = [_image.to(dtype=torch.float16, device=self.device) for _image in image]
+                        else:
+                            image = image.to(dtype=torch.float16, device=self.device)
 
-                    image_tensor = process_images(visual, self._image_processor, self._config)
-                    if type(image_tensor) is list:
-                        image_tensor = [_image.to(dtype=torch.float16, device=self.device) for _image in image_tensor]
-                    else:
-                        image_tensor = image_tensor.to(dtype=torch.float16, device=self.device)
+                        task_type = "image"
 
-                    task_type = "video"
+                    elif type(visual) == str:  # For video task
+                        try:
+                            if self.video_decode_backend == "decord":
+                                frames = self.load_video([visual], self.max_frames_num)
+                            elif self.video_decode_backend == "pyav":
+                                frames = read_video_pyav(visual, num_frm=self.max_frames_num)
+                            frames = self._image_processor.preprocess(frames, return_tensors="pt")["pixel_values"].half().cuda()
+                            image = frames
+                        except Exception as e:
+                            eval_logger.error(f"Error {e} in loading video")
+                            image = None
 
-                elif type(visual[0]) == PIL.Image.Image:
-                    image_tensor = process_images(visual, self._image_processor, self._config)
-                    if type(image_tensor) is list:
-                        image_tensor = [_image.to(dtype=torch.float16, device=self.device) for _image in image_tensor]
-                    else:
-                        image_tensor = image_tensor.to(dtype=torch.float16, device=self.device)
+                        task_type = "video"
+            else:
+                image = None
 
-                    task_type = "image"
+            prompts_input = contexts[0] if isinstance(contexts, list) else contexts
 
-                elif type(visual[0]) == str:
-                    image_tensor = []
-                    try:
-                        if self.video_decode_backend == "decord":
-                            frames = self.load_video(visual, self.max_frames_num)
-                        elif self.video_decode_backend == "pyav":
-                            frames = read_video_pyav(visual[0], num_frm=self.max_frames_num)
-                        frames = self._image_processor.preprocess(frames, return_tensors="pt")["pixel_values"].half().cuda()
-                        image_tensor.append(frames)
-                    except Exception as e:
-                        eval_logger.error(f"Error {e} in loading video")
-                        image_tensor = None
-
-                    task_type = "video"
-
-            if image_tensor is not None and len(image_tensor) != 0 and DEFAULT_IMAGE_TOKEN not in contexts:
-                placeholder_count = len(visual) if isinstance(visual, list) else 1
-                if task_type == "video":
-                    placeholder_count = len(frames) if self.token_strategy == "multiple" else 1
-                image_tokens = [DEFAULT_IMAGE_TOKEN] * placeholder_count
+            if image is not None and len(image) != 0 and DEFAULT_IMAGE_TOKEN not in prompts_input:
+                """
+                Three senarios:
+                1. No image, and there for, no image token should be added.
+                2. image token is already specified in the context, so we don't need to add it.
+                3. image token is not specified in the context and there is image inputs, so we need to add it. In this case, we add the image token at the beginning of the context and add a new line.
+                """
+                if task_type == "image":
+                    image_tokens = [DEFAULT_IMAGE_TOKEN] * len(visuals) if isinstance(visuals, list) else [DEFAULT_IMAGE_TOKEN]
+                elif task_type == "video":
+                    image_tokens = [DEFAULT_IMAGE_TOKEN] * len(frames) if self.token_strategy == "multiple" else [DEFAULT_IMAGE_TOKEN]
                 image_tokens = " ".join(image_tokens)
-                prompts_input = image_tokens + "\n" + contexts
+                prompts_input = image_tokens + "\n" + (contexts[0] if isinstance(contexts, list) else contexts)
             else:
-                prompts_input = contexts
+                question = (contexts[0] if isinstance(contexts, list) else contexts)
 
             if "llama_3" in self.conv_template:
                 conv = copy.deepcopy(conv_templates[self.conv_template])
@@ -329,30 +329,21 @@ class Llava_OneVision(lmms):
             prompt = conv.get_prompt()
 
             input_ids = tokenizer_image_token(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt").unsqueeze(0).to(self.device)
-
-            if type(doc_to_target) == str:
-                continuation = doc_to_target
-            else:
-                continuation = doc_to_target(self.task_dict[task][split][doc_id])
-
-            conv.messages[-1][1] = continuation
-            full_prompt = conv.get_prompt()
-            full_input_ids = tokenizer_image_token(full_prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt").unsqueeze(0).to(self.device)
-
-            labels = full_input_ids.clone()
-            labels[0, : input_ids.shape[1]] = -100
+            labels = input_ids.clone()
+            # Context part no need to calculate for loss
+            labels[0, : contxt_id.shape[1]] = -100
 
             kwargs = {}
             if task_type == "image":
-                kwargs["image_sizes"] = [[v.size[0], v.size[1]] for v in visual] if isinstance(visual, list) else [[visual.size[0], visual.size[1]]]
+                kwargs["image_sizes"] = [[visual.size[0], visual.size[1]] for visual in visuals]
             elif task_type == "video":
                 kwargs["modalities"] = ["video"]
                 self._config.mm_spatial_pool_stride = self.mm_spatial_pool_stride
                 self._config.mm_spatial_pool_mode = self.mm_spatial_pool_mode
 
-            with torch.inference_mode():
-                outputs = self.model(input_ids=full_input_ids, labels=labels, images=image_tensor, use_cache=True, **kwargs)
 
+            with torch.inference_mode():
+                outputs = self.model(input_ids=input_ids, labels=labels, images=image, use_cache=True, **kwargs)
             loss = outputs["loss"]
             logits = outputs["logits"]
             greedy_tokens = logits.argmax(dim=-1)